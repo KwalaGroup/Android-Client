17e1f0ca3e35bf7e7bb71d7dee3ffd3f136e782e
Sijae Brown <sijaebrown@Jordans-MacBook-Pro.local>
2017-02-04 16:09:03 -0600

    login xml finished

---
+++ Kwala/app/src/main/java/com/kwala/app/login/login.java
@@ -0,0 +1,18 @@
+package com.kwala.app.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kwala.app.R;
+
+/**
+ * Created by sijaebrown on 1/24/17.
+ */
+
+public class login extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
