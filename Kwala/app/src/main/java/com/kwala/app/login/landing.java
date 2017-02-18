6cf6cb0b92a36b83cf81dcaab81cd08e62780756
Sijae Brown <sijaebrown@Jordans-MacBook-Pro.local>
2017-02-18 16:04:50 -0600

    Landing finished

---
+++ Kwala/app/src/main/java/com/kwala/app/login/landing.java
@@ -0,0 +1,18 @@
+package com.kwala.app.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kwala.app.R;
+
+/**
+ * Created by sijaebrown on 2/18/17.
+ */
+
+public class landing extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_activity);
+    }
+}
