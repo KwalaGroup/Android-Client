e0c6c0737f379c4dea911cb52b824fb125eea517
Jacob Muchow <jacobamuchow@gmail.com>
2017-02-18 13:49:45 -0600

    Custom view for tab bar, use colors from design.

--- Kwala/app/src/main/java/com/kwala/app/MainActivity.java
+++ Kwala/app/src/main/java/com/kwala/app/MainActivity.java
@@ -1,8 +1,6 @@
 package com.kwala.app;
 
 import android.app.Fragment;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
@@ -10,17 +8,12 @@ import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.kwala.app.filters.FiltersFragment;
 import com.kwala.app.matches.MatchesFragment;
 import com.kwala.app.profile.MyProfileFragment;
 import com.kwala.app.quiz.QuizFragment;
-import com.kwala.app.service.tasks.ITunesRssTask;
-import com.kwala.app.service.tasks.Task;
 import com.kwala.app.settings.SettingsFragment;
-
-import org.json.JSONObject;
 
 /**
  * @author jacobamuchow@gmail.com
@@ -31,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     /*
         References
      */
-    private TabLayout tabLayout;
+    private MainTabLayout tabLayout;
     private ViewPager viewPager;
 
     private PagerAdapter pagerAdapter;
@@ -41,68 +34,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        tabLayout = (TabLayout) findViewById(R.id.main_activity_tab_layout);
+        tabLayout = (MainTabLayout) findViewById(R.id.main_activity_tab_layout);
         viewPager = (ViewPager) findViewById(R.id.main_activity_view_pager);
 
         pagerAdapter = createAdapter();
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
 
-        tabLayout.getTabAt(0).setIcon(R.drawable.brain_icon);
-        tabLayout.getTabAt(1).setIcon(R.drawable.filters_icon);
-        tabLayout.getTabAt(2).setIcon(R.drawable.matches_icon);
-        tabLayout.getTabAt(3).setIcon(R.drawable.profile_icon);
-        tabLayout.getTabAt(4).setIcon(R.drawable.settings_icon);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                updateTabColor(tab, true);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                updateTabColor(tab, false);
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        updateTabColor(tabLayout.getTabAt(0), true);
-
-        new ITunesRssTask().start(new Task.Callback<JSONObject>() {
-            @Override
-            public void onSuccess(JSONObject jsonObject) {
-                Log.d(TAG, "UI success: " + jsonObject.toString());
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                Log.d(TAG, "UI failure", e);
-            }
-        });
-    }
-
-    private void updateTabColor(TabLayout.Tab tab, boolean selected) {
-        if (selected) {
-            int color = Color.WHITE;
-
-            switch (tab.getPosition()) {
-                case 0: color = Color.RED; break;
-                case 1: color = Color.YELLOW; break;
-                case 2: color = Color.BLUE; break;
-                case 3: color = Color.MAGENTA; break;
-                case 4: color = Color.WHITE; break;
-            }
-
-            tab.getIcon().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-            tabLayout.setSelectedTabIndicatorColor(color);
-        } else {
-            tab.getIcon().setColorFilter(null);
-        }
+//        new ITunesRssTask().start(new Task.Callback<JSONObject>() {
+//            @Override
+//            public void onSuccess(JSONObject jsonObject) {
+//                Log.d(TAG, "UI success: " + jsonObject.toString());
+//            }
+//
+//            @Override
+//            public void onFailure(Exception e) {
+//                Log.d(TAG, "UI failure", e);
+//            }
+//        });
     }
 
     private PagerAdapter createAdapter() {
---
+++ Kwala/app/src/main/java/com/kwala/app/MainTabLayout.java
@@ -0,0 +1,79 @@
+package com.kwala.app;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * @author jacobamuchow@gmail.com
+ */
+
+public class MainTabLayout extends TabLayout {
+    private static final String TAG = MainTabLayout.class.getSimpleName();
+    
+    public MainTabLayout(Context context) {
+        super(context);
+    }
+
+    public MainTabLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MainTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setupWithViewPager(@Nullable ViewPager viewPager) {
+        super.setupWithViewPager(viewPager);
+        
+        getTabAt(0).setIcon(R.drawable.brain_icon);
+        getTabAt(1).setIcon(R.drawable.filters_icon);
+        getTabAt(2).setIcon(R.drawable.matches_icon);
+        getTabAt(3).setIcon(R.drawable.profile_icon);
+        getTabAt(4).setIcon(R.drawable.settings_icon);
+
+        addOnTabSelectedListener(new OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(Tab tab) {
+                updateTabColor(tab, true);
+            }
+
+            @Override
+            public void onTabUnselected(Tab tab) {
+                updateTabColor(tab, false);
+            }
+
+            @Override
+            public void onTabReselected(Tab tab) {
+
+            }
+        });
+
+        updateTabColor(getTabAt(0), true);
+    }
+
+    private void updateTabColor(Tab tab, boolean selected) {
+        if (selected) {
+            int color = Color.WHITE;
+
+            switch (tab.getPosition()) {
+                case 0: color = ContextCompat.getColor(getContext(), R.color.kPink); break;
+                case 1: color = ContextCompat.getColor(getContext(), R.color.kYellow); break;
+                case 2: color = ContextCompat.getColor(getContext(), R.color.kLightBlue); break;
+                case 3: color = ContextCompat.getColor(getContext(), R.color.kOrange); break;
+                case 4: color = ContextCompat.getColor(getContext(), R.color.kWhite); break;
+            }
+
+            tab.getIcon().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+            setSelectedTabIndicatorColor(color);
+        } else {
+            tab.getIcon().setColorFilter(null);
+        }
+    }
+}
