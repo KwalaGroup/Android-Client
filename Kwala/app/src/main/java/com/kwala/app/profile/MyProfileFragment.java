ed69aada56f85b572d414a856e774328db87fb79
Jacob Muchow <jacobamuchow@gmail.com>
2017-03-02 16:16:05 -0600

    First pass with working image upload.

--- Kwala/app/src/main/java/com/kwala/app/profile/MyProfileFragment.java
+++ Kwala/app/src/main/java/com/kwala/app/profile/MyProfileFragment.java
@@ -5,18 +5,22 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.kwala.app.R;
 import com.kwala.app.helpers.BaseFragment;
+import com.kwala.app.service.DataStore;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
 
-import pl.aprilapps.easyphotopicker.DefaultCallback;
 import pl.aprilapps.easyphotopicker.EasyImage;
 import pl.tajchert.nammu.Nammu;
 import pl.tajchert.nammu.PermissionCallback;
@@ -58,7 +62,10 @@ public class MyProfileFragment extends BaseFragment {
                 Nammu.askForPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionCallback() {
                     @Override
                     public void permissionGranted() {
-                        EasyImage.openChooserWithGallery(MyProfileFragment.this, "", 0);
+//                        EasyImage.openChooserWithDocuments(MyProfileFragment.this, "", 0);
+
+                        Intent intent = CropImage.getPickImageChooserIntent(getActivity());
+                        startActivityForResult(intent, CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE);
                     }
 
                     @Override
@@ -74,12 +81,43 @@ public class MyProfileFragment extends BaseFragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        EasyImage.handleActivityResult(requestCode, resultCode, data, getActivity(), new DefaultCallback() {
-            @Override
-            public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE) {
+            Uri imageUri = CropImage.getPickImageResultUri(getActivity(), data);
 
-                profileImageView.setImageURI(Uri.fromFile(imageFile));
+            Intent intent = CropImage.activity(imageUri)
+                    .setAspectRatio(1, 1)
+                    .setFixAspectRatio(true)
+                    .setCropShape(CropImageView.CropShape.OVAL)
+                    .getIntent(getActivity());
+
+            startActivityForResult(intent, CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+
+        else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            Log.d(TAG, "activity result for crop");
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (result.getError() != null) {
+                Log.e(TAG, "Error cropping image", result.getError());
+                Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+            } else {
+                profileImageView.setImageURI(result.getUri());
+                Log.d(TAG, "image set");
+                Log.d(TAG, "bitmap null: " + (result.getBitmap() == null));
+
+                File file = new File(result.getUri().getPath());
+                DataStore.getInstance().getNetworkStore().uploadImage(file);
             }
-        });
+        }
+
+//        EasyImage.handleActivityResult(requestCode, resultCode, data, getActivity(), new DefaultCallback() {
+//            @Override
+//            public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
+//
+////                CropImage.
+////                profileImageView.setImageURI(Uri.fromFile(imageFile));
+//            }
+//        });
     }
 }
--- Kwala/app/src/main/java/com/kwala/app/service/NetworkStore.java
+++ Kwala/app/src/main/java/com/kwala/app/service/NetworkStore.java
@@ -3,6 +3,9 @@ package com.kwala.app.service;
 import android.util.Log;
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -10,7 +13,9 @@ import com.kwala.app.KwalaApplication;
 import com.kwala.app.service.endpoints.Endpoint;
 import com.kwala.app.service.endpoints.EndpointRequest;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
@@ -43,7 +48,13 @@ public class NetworkStore {
         s3Client = new AmazonS3Client(credentialsProvider);
         transferUtility = new TransferUtility(s3Client, application);
 
-        //TODO: uploadz
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                boolean exists = s3Client.doesBucketExist("kwala-images");
+//                Log.e(TAG, "bucket exists: " + exists);
+//            }
+//        }).start();
     }
 
     public <T> EndpointRequest<T> performRequest(final Endpoint<T> endpoint, final EndpointRequest.Callback<T> callback) {
@@ -71,5 +82,29 @@ public class NetworkStore {
         okHttpClient.newCall(request).enqueue(responseCallback);
 
         return new EndpointRequest<>(endpoint, callback);
+    }
+
+    public void uploadImage(File file) {
+
+        String imageId = UUID.randomUUID().toString();
+
+        TransferObserver observer = transferUtility.upload("kwala-uploads", imageId, file);
+
+        observer.setTransferListener(new TransferListener() {
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                Log.d(TAG, "" + id + ": " + state.name());
+            }
+
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                Log.d(TAG, "" + id + ": " + bytesCurrent + " / " + bytesTotal);
+            }
+
+            @Override
+            public void onError(int id, Exception ex) {
+                Log.e(TAG, "Error uploading image: " + id, ex);
+            }
+        });
     }
 }
