9357e43674e2fcd5fd4b3f1819727532ad20dddc
Jacob Muchow <jacobamuchow@gmail.com>
2017-03-07 16:47:09 -0600

    Updated support libs, added Location Services lib, access fine location for user.

--- Kwala/app/src/main/java/com/kwala/app/MainActivity.java
+++ Kwala/app/src/main/java/com/kwala/app/MainActivity.java
@@ -1,12 +1,20 @@
 package com.kwala.app;
 
+import android.*;
+import android.Manifest;
 import android.app.Fragment;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.kwala.app.filters.FiltersFragment;
 import com.kwala.app.helpers.BaseActivity;
 import com.kwala.app.matches.MatchesFragment;
@@ -14,11 +22,16 @@ import com.kwala.app.profile.MyProfileFragment;
 import com.kwala.app.quiz.QuizFragment;
 import com.kwala.app.settings.SettingsFragment;
 
+import pl.tajchert.nammu.Nammu;
+import pl.tajchert.nammu.PermissionCallback;
+
 /**
  * @author jacobamuchow@gmail.com
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    GoogleApiClient mGoogleApiClient;
 
     /*
         References
@@ -41,6 +54,27 @@ public class MainActivity extends BaseActivity {
         tabLayout.setupWithViewPager(viewPager);
 
         viewPager.setCurrentItem(2);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     private PagerAdapter createAdapter() {
@@ -71,5 +105,35 @@ public class MainActivity extends BaseActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Nammu.askForPermission(this, Manifest.permission.ACCESS_FINE_LOCATION, new PermissionCallback() {
+            @Override
+            public void permissionGranted() {
+                if (mGoogleApiClient.isConnected()) {
+                    Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                    if (mLastLocation != null) {
+                        Log.d(TAG, "" + mLastLocation.getLatitude() + "," + mLastLocation.getLongitude());
+                    }
+                }
+            }
+
+            @Override
+            public void permissionRefused() {
+                // :(
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "connection failed: " + connectionResult.toString());
     }
 }
