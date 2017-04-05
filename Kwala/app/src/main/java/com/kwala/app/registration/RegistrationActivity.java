d244142add40835fbd3ebd02bea4aab562d93da4
Jacob Muchow <jacobamuchow@gmail.com>
2017-04-05 16:10:12 -0500

    Show spinner / disable continue button on first reg screen.

--- Kwala/app/src/main/java/com/kwala/app/registration/RegistrationActivity.java
+++ Kwala/app/src/main/java/com/kwala/app/registration/RegistrationActivity.java
@@ -3,14 +3,18 @@ package com.kwala.app.registration;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.kwala.app.R;
+import com.kwala.app.helpers.SimpleTextWatcher;
 import com.kwala.app.helpers.navigation.BaseActivity;
 import com.kwala.app.helpers.views.KwalaEditText;
+import com.kwala.app.helpers.views.KwalaProgressSpinner;
 import com.kwala.app.service.RegistrationData;
 import com.kwala.app.service.tasks.Task;
 import com.kwala.app.service.tasks.auth.EmailAvailabilityTask;
@@ -24,7 +28,10 @@ public class RegistrationActivity extends BaseActivity {
 
     private KwalaEditText emailEditText;
     private KwalaEditText passwordEditText;
+    private KwalaProgressSpinner progressSpinner;
     private Button continueButton;
+
+    private boolean networkPending = false;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, RegistrationActivity.class);
@@ -37,10 +44,45 @@ public class RegistrationActivity extends BaseActivity {
 
         emailEditText = (KwalaEditText) findViewById(R.id.registration_email_text);
         passwordEditText = (KwalaEditText) findViewById(R.id.registration_password_text);
+        progressSpinner = (KwalaProgressSpinner) findViewById(R.id.registration_progress_spinner);
         continueButton = (Button) findViewById(R.id.registration_continue_button);
 
+        emailEditText.addTextChangedListener(textWatcher);
+        passwordEditText.addTextChangedListener(textWatcher);
         continueButton.setOnClickListener(continueClickListener);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateViews();
+    }
+
+    private boolean isFormComplete() {
+        return !TextUtils.isEmpty(emailEditText.getTextTrimmed())
+                && !TextUtils.isEmpty(passwordEditText.getTextTrimmed());
+    }
+
+    private void updateViews() {
+        if (emailEditText == null) {
+            return;
+        }
+
+        continueButton.setEnabled(isFormComplete() && !networkPending);
+
+        progressSpinner.setVisibility(networkPending ? View.VISIBLE : View.GONE);
+    }
+
+    /**
+     * Listeners
+     */
+    private final SimpleTextWatcher textWatcher = new SimpleTextWatcher() {
+        @Override
+        public void afterTextChanged(Editable s) {
+            super.afterTextChanged(s);
+            updateViews();
+        }
+    };
 
     private final View.OnClickListener continueClickListener = new View.OnClickListener() {
         @Override
@@ -55,6 +97,8 @@ public class RegistrationActivity extends BaseActivity {
             new EmailAvailabilityTask(email).start(new Task.Callback<Boolean>() {
                 @Override
                 public void onSuccess(Boolean isAvailable) {
+                    networkPending = false;
+
                     if (isAvailable) {
                         startActivity(RegistrationActivity2.newIntent(RegistrationActivity.this));
                     } else {
@@ -65,8 +109,13 @@ public class RegistrationActivity extends BaseActivity {
                 @Override
                 public void onFailure(Exception e) {
                     Log.e(TAG, "Error", e);
+                    networkPending = false;
+                    updateViews();
                 }
             });
+
+            networkPending = true;
+            updateViews();
         }
     };
 }
