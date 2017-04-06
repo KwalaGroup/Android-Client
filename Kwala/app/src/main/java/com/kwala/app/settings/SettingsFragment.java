3bb9956dab7688bbd854ff56fe7ee4d78dcda85d
Jacob Muchow <jacobamuchow@gmail.com>
2017-04-05 22:59:47 -0500

    Show version name and number in settings.

--- Kwala/app/src/main/java/com/kwala/app/settings/SettingsFragment.java
+++ Kwala/app/src/main/java/com/kwala/app/settings/SettingsFragment.java
@@ -8,11 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.kwala.app.BuildConfig;
 import com.kwala.app.R;
 import com.kwala.app.login.LandingActivity;
 import com.kwala.app.service.DataStore;
 import com.kwala.app.service.tasks.auth.LogoutTask;
+
+import java.util.Locale;
 
 /**
  * @author jacobamuchow@gmail.com
@@ -35,7 +39,10 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        TextView versionTextView = (TextView) view.findViewById(R.id.settings_version_text);
         Button signOutButton = (Button) view.findViewById(R.id.settings_logout_button);
+
+        versionTextView.setText(String.format(Locale.US, "%s (%d)", BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
 
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
